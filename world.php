8efd6249308dc982804996a0b13072fc2eb77919
Ubuntu <ubuntu@ip-172-31-92-40.ec2.internal>
2019-11-24 02:55:25 +0000

    Edited world.php

--- world.php
+++ world.php
@@ -1,19 +1,60 @@
-<?php
+<?php 
+
 $host = getenv('IP');
 $username = 'Domonique_E';
-$password = 'Info2180_7';
+$password = 'Info2180-lab7';
 $dbname = 'world';
 
-$country=$_GET['country'];
+$country= $_GET['country']; 
+$city= $_GET['city']; 
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");  
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);  
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'"); 
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
 
- 
-?> 
 
-<ul> 
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+$stmt2 = $conn->query("SELECT c.district as district, c.name as city, c.country_code, cs.name as country, c.population as population 
+FROM cities c join countries cs on c.country_code=cs.code WHERE cs.name='%$context%'");
+$context=$stmt2->fetchAll(PDO::FETCH_ASSOC);
+
+?>  
+
+<?php if(!(isset($_GET['context']))):?>
+<table style="border:1px solid black">
+    <tr>
+        <th>Country Name</th>
+        <th>Continent</th> 
+        <th>Independence</th>
+        <th>Head of State</th>
+    </tr> 
+<tbody>
+<?php foreach($results as $row): ?>
+  <tr>
+      <td><?=$row['name']?></td> 
+      <td><?=$row['continent']?></td>
+      <td><?=$row['independence_year']?></td>
+      <td><?=$row['head_of_state']?></td>
+  </tr>
+<?php endforeach; ?>  
+</tbody>
+</table>  
+
+<?php else : ?> 
+
+<table style= "border:1px solid black">
+    <tr> 
+      <th>Name</th> 
+      <th>District</th> 
+     <th>Population</th>
+   </tr>  
+ <tbody>
+<?php foreach ($context as $row): ?>
+    <tr>
+        <td><?=$row['city']?></td>
+        <td><?=$row['district']?></td>
+        <td><?=$row['population']?></td>
+    </tr>
+<?php endforeach; ?> 
+</tbody>
+</table> 
+<?php endif; ?>
